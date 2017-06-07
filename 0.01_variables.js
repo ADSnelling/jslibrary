75fd20d7807ddd8b5585fc43241594d513940867
Snail <adsnelling@comcast.net>
2017-06-07 10:47:57 -0400

    js stuff

--- 0.01_variables.js
+++ 0.01_variables.js
@@ -21,5 +21,20 @@ console.log(street + ", \n" + state + " " + zip + ".");
 console.log("Ben says, \"Good morrow to you sir\" ");
 console.log("it's going to be a great day");
 
+var yesOrNo = true;
 
+console.log(yesOrNo);
+console.log("are you awake? " + yesOrNo);
+// + add
+// - sub
+//  *multiple
+// / division
+// % modulus
+//  = assignment
+//  == compare
+// 
+console.log("The answer to 10 % 3 is: " , 10 % 3); //two arguments
+console.log("The answer to 10 % 3 is: " + 10 % 3); //one argument
+console.log(typeof("snail")); //one argument
+console.log(typeof(yesOrNo)); //one argument
 
