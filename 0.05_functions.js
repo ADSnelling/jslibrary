19e891481a0087967619b290d4bb596ed2d5a577
Snail <adsnelling@comcast.net>
2017-06-07 15:19:24 -0400

    functions for mul/mod/div/sub

--- 0.05_functions.js
+++ 0.05_functions.js
@@ -43,12 +43,39 @@ function add(num1, num2){
 add(23, 45);
 
 
+function sum (num1, num2){
+	return num1 + num2;
+}
+
+sum(56, 78);
+
+var numbersAdded = sum(65, 45);
+
+console.log(sum(43, 12));
+
+console.log(numbersAdded);
+
+function multilply(num1, num2){
+	return num1 * num2;
+}
+
+function divide(num1, num2){
+	return num1 / num2;
+}
+
+function remain(num1, num2){
+	return num1 % num2;
+}
+
+function subby(num1, num2){
+	return num1 - num2;
+}
 
 
-
-
-
-
+console.log(multilply(3,4));
+console.log(divide(100,5));
+console.log(remain(60,4));
+console.log(subby(103,34));
 
 
 
