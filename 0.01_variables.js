09f679c03c24cc7b9b84117421ed70bbe5736244
Snail <adsnelling@comcast.net>
2017-06-07 09:55:46 -0400

    smashing dem variables together

--- 0.01_variables.js
+++ 0.01_variables.js
@@ -1,11 +1,23 @@
 //Declaration 
 var name;
+var lastName;
 console.log(name);
 // initialize
 // data type of string
-name = "snail";
+name = "Snail";
+lastName = "Snaileroo"
 console.log(name);
 
+console.log(name + " " + lastName);
+
+// challenge print out address in 3 variables, street state and zip
+// add together
+
+var street = "1003 Summer Hill";
+var state = "Carmel, IN.";
+var zip = "46032";
+
+console.log("Your address is " + street + ", " + state + " " + zip + ".");
 
 
 
