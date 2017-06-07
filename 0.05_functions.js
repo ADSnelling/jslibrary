1f16d069c65d83883cbf49164d54aeb768ca6005
Snail <adsnelling@comcast.net>
2017-06-07 15:00:46 -0400

    functions

---
+++ 0.05_functions.js
@@ -0,0 +1,74 @@
+//functions
+
+// function Name_of_Function(){
+// 
+// }
+
+
+function greeting(name){
+
+	var name;
+	console.log(name + " says \"Hello there buddy ol pal\"");
+
+}
+
+var stringOne = "ayyyyyyy";
+
+greeting(stringOne);
+
+function getWeather(){
+
+	console.log("Todays weather is weather my dude");
+
+}
+
+getWeather();
+
+
+function greetingTwo(nameTwo){
+
+	var nameTwo;
+	console.log(nameTwo + " says \"Hello there buddy ol pal\"");
+
+}
+
+function zapatos(brand){
+
+	console.log("man those " + brand + " are dapper");
+}
+
+zapatos("vans");
+
+function add(num1, num2){
+
+	console.log(num1 + num2);
+
+}
+
+add(23, 45);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
