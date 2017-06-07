2b96592b690030b49c3c160c0fdb4c15b6519fd5
Snail <adsnelling@comcast.net>
2017-06-07 14:10:26 -0400

    while and for loop

--- 0.04_conditionals.js
+++ 0.04_conditionals.js
@@ -124,6 +124,34 @@ for (i = 1; i <= 100; i++) {
 
 
 
+var num = 1;
+
+while(num < 101){
+
+	if(num % 3 == 0 && num % 5 == 0){
+
+		console.log("FizzBuzz");
+
+	}else if (num % 3 == 0) {
+
+		console.log("Fizz");
+
+	}else if (num % 5 == 0) {
+
+		console.log("Buzz");
+
+	}else{
+
+		console.log(num)
+
+	}//end if
+
+	num += 1;
+
+}
+
+
+
 
 
 // var
