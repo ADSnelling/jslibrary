4ab5de13fb73f34a17267db24ecf33b6d45af934
Snail <adsnelling@comcast.net>
2017-06-07 13:10:05 -0400

    if/else

---
+++ 0.04_conditionals.js
@@ -0,0 +1,66 @@
+// conditionals
+// && (and logical)
+// || (or logical)
+// < greater than
+// > less than
+// >= greater than equal to
+// <= less than equal to
+// == equal to
+// === Strictly equal(type and value)
+// ! not		
+// != not equal to
+
+console.log(1 > 3);
+console.log(10 != 10);
+var name = "snail";
+console.log(name == "Snail");
+console.log(name === "Snail");
+
+console.log(6 == "6");
+console.log(6 === "6");
+var orNah = true;
+// if statements
+if (242 % 2 === 0) {
+
+	console.log("suh bruhh");	
+
+} else {
+
+	console.log("nah breh");
+	
+}
+
+var userName = "admin";
+var password = "password1";
+
+// if (userName == "admin") {
+
+// 	console.log("you have been granted access!");
+	
+// }
+
+if (userName == "admin" && password == "password") {
+
+	console.log("you have been granted access!");
+	
+}else{
+	if(userName != "admin"){
+		console.log("username is wrong breh");
+		
+	}else{
+		console.log("password is wrong breh");
+
+	}
+}
+
+
+
+
+
+
+// console.log();
+// console.log();
+// var
+// var
+// var
+
