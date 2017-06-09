b46846d09262b062c16d112d2dafd42ac5b0f9c6
Snail <adsnelling@comcast.net>
2017-06-09 15:00:39 -0400

    started dom

---
+++ 0.09_DOM.js
@@ -0,0 +1,30 @@
+function Hotel(name, rooms, booked){
+
+	this.name = name;
+	this.rooms = rooms;
+	this.booked = booked;
+	this.checkVac = function(){
+
+		return this.rooms - this.booked;
+
+	};
+
+}
+
+var rri = new Hotel("Red Roof Inn", 250, 230);
+var alex = new Hotel("The Alexander", 500, 150);
+
+var hotels = [rri, alex];
+
+function showAllHotelsInfo(){
+	for(var i in hotels){
+		
+		var hotelName = hotels[i].name;
+		var hotelHtmlText = document.createTextNode(hotelName);
+		var listItem = document.createElement("li");
+		listItem.appendChild(hotelHtmlText);
+		document.getElementById("hotels").appendChild(listItem);
+	}
+}
+showAllHotelsInfo();
+
