f8fba4286b51c14d8ddb09879a2565546ecc0527
Snail <adsnelling@comcast.net>
2017-06-19 13:50:53 -0400

    yeah

--- 0.11_closures.js
+++ 0.11_closures.js
@@ -4,11 +4,44 @@ closures essentially functions
 
 */
 var init = function(){
+
 	var name = "Summer";
 
 	function displayName(){
+
 		console.log(name);
-	}
+
+	}//close displayName
+
 	displayName();
-}
+
+}//close init
+
 init();
+
+
+var friend = {};
+
+var setAge = function(myAge){
+
+	var bitrhday = '5/16/2017';
+
+	return {
+
+		getAge: function(){
+
+			console.log(myAge);
+			return myAge;
+
+		}//close getAge
+
+	}//close return
+
+}//close setAge
+
+
+
+friend.age = setAge(55);
+console.log(friend.age);
+console.log(friend.age.getAge());
+console.log(friend.birthday);
---
+++ 0.12_iffe.js
@@ -0,0 +1,27 @@
+// immediately-invoked function expression
+
+(function printNum(){
+	var a=5;
+	console.log(a);
+})();
+
+// challenge create a iffe that counts from 1-10
+
+(function oneTen(){
+	for(i=1;i<11;i++){console.log(i)};
+})();
+
+
+var smartCar = {};
+var smartCarMPG = function(totalMiles, totalGallons){
+	return{
+		calculateMpg : function(){
+			console.log("MPG: ");
+			return totalMiles / totalGallons;
+		};
+	};
+};
+
+smartCar.mpg = smartCarMPG(100, 15);
+console.log(smartCar.mpg);
+console.log(smartCar.mpg.calculateMpg());
